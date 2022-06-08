7cdee497bb7bd7055a171c8b9bae701f50e6920e
Stephanie Simms <88897284+StephSimms@users.noreply.github.com>
2022-06-08 18:28:20 -0300

    Add project files.

---
+++ Enums_Project/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("What is the current day of the week?");
+
+                Weekday today = (Weekday)Enum.Parse(typeof(Weekday), Console.ReadLine());
+
+                Console.WriteLine("The current day of the week is: " + today);
+            }
+
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+
+    public enum Weekday
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+        
+    }
+
+}
